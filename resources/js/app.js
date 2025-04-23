3aaa97a690eed847548d1c1cd47b081b346f67a8
AdrianMoraJ <amora19072004@gmail.com>
2025-04-23 19:11:42 +0200

    Cambio en los ficheros app.js y web.php

---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+import '../css/app.css';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from 'ziggy-js';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Autonovaparts';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    //dinámico
+    resolve: (name) => import(`./pages/${name}.vue`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
