ab7913208c2edf6362b8b32c783bfa392ae7ab45
DanielMuñoz <daniel.munoz116@educa.madrid.org>
2025-05-13 18:02:18 +0200

    New component TopBar created

---
+++ resources/js/components/TopBar.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="flex h-16 justify-around border-b-2 border-b-blue-400">
+        <img :src="imageTitle" height="60" class="w-auto object-contain" alt="AutonovaParts" />
+        <div class="flex h-full">
+            <button @click="loadAbout" class="h-full w-28 text-center">
+                <div
+                    :class="{
+                        'border-t-4 border-t-blue-400 text-blue-400': valueSelected === 'about',
+                    }"
+                    class="flex h-full w-full items-center justify-center transition duration-300 ease-in-out hover:border-t-4 hover:border-t-blue-400 hover:text-blue-400"
+                >
+                    <span class="roboto">Conócenos</span>
+                </div>
+            </button>
+            <button @click="loadServices" class="h-full w-28 text-center">
+                <div
+                    :class="{
+                        'border-t-4 border-t-blue-400 text-blue-400': valueSelected === 'services',
+                    }"
+                    class="flex h-full w-full items-center justify-center transition duration-300 ease-in-out hover:border-t-4 hover:border-t-blue-400 hover:text-blue-400"
+                >
+                    <span class="roboto">Servicios</span>
+                </div>
+            </button>
+            <button @click="loadItems" class="h-full w-28 text-center">
+                <div
+                    :class="{
+                        'border-t-4 border-t-blue-400 text-blue-400': valueSelected === 'items',
+                    }"
+                    class="flex h-full w-full items-center justify-center transition duration-300 ease-in-out hover:border-t-4 hover:border-t-blue-400 hover:text-blue-400"
+                >
+                    <span class="roboto">Catálogo</span>
+                </div>
+            </button>
+            <button @click="loadPerfil" class="h-full w-28 text-center">
+                <div
+                    :class="{
+                        'border-t-4 border-t-blue-400 text-blue-400': valueSelected === 'perfil',
+                    }"
+                    class="flex h-full w-full items-center justify-center transition duration-300 ease-in-out hover:border-t-4 hover:border-t-blue-400 hover:text-blue-400"
+                >
+                    <span class="roboto">Perfil</span>
+                </div>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const emit = defineEmits(['loadAbout', 'loadServices', 'loadItems', 'loadPerfil']);
+const imageTitle = ref('/images/autonovaparts_title.jpg');
+const valueSelected = ref('');
+
+const loadAbout = () => {
+    emit('loadAbout');
+    valueSelected.value = 'about';
+};
+
+const loadServices = () => {
+    emit('loadServices');
+    valueSelected.value = 'services';
+};
+
+const loadItems = () => {
+    emit('loadItems');
+    valueSelected.value = 'items';
+};
+
+const loadPerfil = () => {
+    emit('loadPerfil');
+    valueSelected.value = 'perfil';
+};
+</script>
+
+<style>
+.roboto {
+    font-family: 'Roboto', sans-serif;
+    font-optical-sizing: auto;
+    font-style: normal;
+    font-variation-settings: 'wdth' 100;
+}
+</style>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>Dashboard</div>
+    <TopBar @load-about="loadAbout" @load-services="loadServices" @load-items="loadItems" @load-perfil="loadPerfil"></TopBar>
 </template>
+
+<script setup>
+import TopBar from '@/components/TopBar.vue';
+
+const loadAbout = () => {
+    console.log('about');
+};
+
+const loadServices = () => {
+    console.log('service');
+};
+
+const loadItems = () => {
+    console.log('items');
+};
+
+const loadPerfil = () => {
+    console.log('perfil');
+};
+</script>
