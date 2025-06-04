d232defe3717868d393daad86e5b8bbf9d9b731d
DanielMuñoz <daniel.munoz116@educa.madrid.org>
2025-06-04 23:01:27 +0200

    Created at items fixed

--- app/Http/Controllers/PartController.php
+++ app/Http/Controllers/PartController.php
@@ -19,6 +19,8 @@ class PartController extends Controller
                 'stock',
                 'image_url',
                 'is_active',
+                'created_at',
+                'updated_at',
                 'category_id'
             ])
             ->get();
